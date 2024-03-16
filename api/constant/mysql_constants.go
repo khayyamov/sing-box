c78bdb68f7adb900b5ac193f8e879ffb42b617ae
khayyamov <khayyamov@gmail.com>
2024-03-16 13:16:34 +0330

    beta04

---
+++ api/constant/mysql_constants.go
@@ -0,0 +1,9 @@
+package constant
+
+const DbUsername = "root"
+const DbPassword = "@Gold1380"
+const DbConnection = "tcp"
+const DbHost = "localhost"
+const DbPort = "3306"
+const DbName = "users_db"
+const DbCharset = "utf8"
