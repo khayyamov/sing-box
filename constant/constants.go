232948b6f9d61502718172b8735b389476c27b9d
khayyamov <72504271+khayyamov@users.noreply.github.com>
2024-06-07 16:45:16 +0330

    Update constants.go
    Signed-off-by: khayyamov <72504271+khayyamov@users.noreply.github.com>

--- constant/constants.go
+++ constant/constants.go
@@ -2,6 +2,6 @@ package constant
 
 var (
 	ENCRYPTED_CONFIG = false
-	ENCRYPT_KEY      = "RGVqPPTLXfiyApgYHUEujnvF0uu1ui0="
-	ENCRYPT_KEY_IV   = "QuAyl3AyTo5UZB0="
+	ENCRYPT_KEY      = ""
+	ENCRYPT_KEY_IV   = ""
 )
